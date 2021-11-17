c717332022af2c201eb64e39ab44adee88f907ca
dylanfisher <hi@dylanfisher.com>
2021-11-17 13:03:10 -0500

    Update images script with tests from Pi

--- README.md
+++ README.md
@@ -5,7 +5,33 @@
 - [800×480, 7.5inch E-Ink display HAT for Raspberry Pi](https://www.waveshare.com/7.5inch-e-paper-hat.htm)
 - [Raspberry Pi Zero WH](https://www.raspberrypi.com/products/raspberry-pi-zero/)
 
-
 ## Inspiration
 
 The inspiration and initial code for this project comes from this project: https://github.com/TomWhitwell/SlowMovie
+
+## Installation
+
+Install depdendencies:
+
+- `sudo apt install python3-pip`
+- `pip3 install git+https://github.com/waveshare/e-Paper.git#egg=waveshare-epd&subdirectory=RaspberryPi_JetsonNano/python`
+- `pip3 install pillow`
+- `pip3 install git+https://github.com/robweber/omni-epd.git#egg=omni-epd`
+
+## Scratchpad
+
+### Imagemagick conversions
+
+Resize and crop images to fit the 2x2 EPD display grid
+
+`find . -maxdepth 1 -iname "*.jpg" | xargs -L1 -I{} convert -verbose -thumbnail 1600x960^ -gravity center -extent 1600x960 -unsharp 0x.5 "{}" ~/_resized/"{}"`
+
+Potential idea for cropping into a single point of the image and displaying a particular zoomed-in point on each screen
+
+`find . -maxdepth 1 -iname "*.jpg" | xargs -L1 -I{} convert -extent 800x480 -verbose -quality 50 "{}" ~/_resized/"{}"`
+
+### rsync
+
+Sync resized source images to the Pi host
+
+`rsync -azP ~/_resized/ pi@10.0.0.5:~/Pictures`
--- images.py
+++ images.py
@@ -12,7 +12,11 @@ import sys
 import random
 import signal
 import ffmpeg
-from PIL import Image, ImageEnhance
+import math
+import random
+import numpy as np
+from PIL import Image, ImageFont, ImageDraw
+from datetime import datetime
 
 # Ensure this is the correct import for your particular screen
 from waveshare_epd import epd7in5_V2 as epd_driver
@@ -30,12 +34,27 @@ def is_supported_filetype(file):
     _, ext = os.path.splitext(file)
     return ext.lower() in [".jpeg", ".jpg"]
 
+# Used to generate a gradient
+def interpolate(f_co, t_co, interval):
+  det_co =[(t - f) / interval for f , t in zip(f_co, t_co)]
+  for i in range(interval):
+    yield [round(f + det * i) for f, det in zip(f_co, det_co)]
+
+# Configure variables
+today = datetime.now()
+current_hour = int(today.strftime("%H"))
+
 # Initialize the EPD driver
 epd = epd_driver.EPD()
 width = epd.width   # 800
 height = epd.height # 480
-
 epd.init()
+
+# Clear the screen between the hours of 2am and 8am to prevent potential burn-in
+if current_hour > 2 and current_hour < 8:
+  epd.Clear()
+  epd.sleep()
+  sys.exit()
 
 # Ensure this is the correct path to your files directory
 file_dir = os.path.join(os.path.expanduser('~'), "Pictures")
@@ -49,21 +68,33 @@ if not files:
     sys.exit()
 current_file = os.path.join(file_dir, random.choice(files))
 
-# Open image in PIL
-pil_img = Image.open(current_file)
-
-# Resize the image so that it covers the size of the display, without changing the aspect ratio
-img_height, img_width = pil_img.size
-ratio = (width/float(pil_img.size[0]))
-new_height = int((float(img_width)*float(ratio)))
-pil_img = pil_img.resize((width, new_height), Image.NEAREST)
-
 # Create an empty PIL image canvas in which to paste the current image
 canvas_color = (255, 255, 255)
-canvas = Image.new(pil_img.mode, (width, height), canvas_color)
-placement_x = 0
-placement_y = int((new_height - height) / -2.0)
-canvas.paste(pil_img, (placement_x, placement_y))
+canvas = Image.new('RGB', (width, height), canvas_color)
+
+if False and today.strftime("%M") == '00':
+  font = ImageFont.truetype(os.path.join(os.getcwd(), "fonts/TestFoundersGroteskX-Condensed-Light.otf"), 150)
+  text_buffer = Image.new('RGB', (width, height), canvas_color)
+  drawing = ImageDraw.Draw(text_buffer)
+  text_lines = [today.strftime("%A, %B %-d"), today.strftime("%Y"), today.strftime("%-I %M %p")]
+  drawing.text((0, 0), "\n".join(text_lines), (0, 0, 0), font=font)
+  canvas.paste(text_buffer, (0, 0))
+elif random.randint(0,10) == 5:
+  # Draw a circular gradient from the center outward
+  x_axis = np.linspace(-1, 1, height)[:, None] * random.randint(0,255)
+  y_axis = np.linspace(-1, 1, width)[None, :] * random.randint(0,255)
+  gradient_array = np.sqrt(x_axis ** 2 + y_axis ** 2)
+  gradient_image = Image.fromarray(np.uint8(gradient_array))
+  canvas.paste(gradient_image, (0, 0))
+else:
+  # Open image in PIL
+  pil_img = Image.open(current_file)
+  # Resize the image so that it covers the size of the display, without changing the aspect ratio
+  img_width, img_height = pil_img.size
+  ratio = (width/float(pil_img.size[0]))
+  new_height = int((float(img_height)*float(ratio)))
+  pil_img = pil_img.resize((width, new_height), Image.NEAREST)
+  canvas.paste(pil_img, (0, int((new_height - height) / -2.0)))
 
 # Update the EPD display with our image
 epd.display(epd.getbuffer(canvas))
